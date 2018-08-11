e2ecc72b23fc9282ab44b52c265c3abb6f8932a1
Vishvang <vishvangp.8@gmail.com>
2018-08-11 16:23:32 -0400

    Instead of mongodb create _id:ObjectID for us in document, We can access ObjectID constructor from mongodb library to create _id property by our self using Object destructuring.

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,16 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+//ES6 Object Destructuring Feature
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true},
-(err, client) => {
+var {MongoClient, ObjectID} = require('mongodb');
+
+// To insert _id into document we can specify by ourself also.
+var obj = new ObjectID();
+console.log('ObjectID:',obj);
+
+MongoClient.connect(
+    'mongodb://localhost:27017/TodoApp',
+    {useNewUrlParser: true},
+    (err, client) => {
     if(err) {
         return console.log('Unable to coonect with MongoDB server');
     }
@@ -10,30 +19,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true},
     // Database instance.
     const db = client.db('TodoApp');
     
-    //Create one new Collection Todos and Insert one single record
-    db.collection('Todos').insertOne({
-        Text:'Something to do',
-        completed: false
-    },(err, result) => {
-        if(err){
-            console.log('Unable to insert document into Todos Collection', err);
-        }
+    // Create one new Collection Todos and Insert one single record
+    // db.collection('Todos').insertOne({
+    //     Text:'Something to do',
+    //     completed: false
+    // },(err, result) => {
+    //     if(err){
+    //         console.log('Unable to insert document into Todos Collection', err);
+    //     }
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
 
-    //Create one new Collection Users and Insert one single record
+    // Create one new Collection Users and Insert one single record
     db.collection('Users').insertOne({
-        name: 'Vish',
-        age: 27,
-        location: 'Tampa'
+        _id: obj,
+        name: 'Vang',
+        age: 15,
+        location: 'Orlando'
     },(err, result) => {
         if(err){
             console.log('Unable to insert document into Todos Collection', err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
+        console.log('TimeStamp from _id:',result.ops[0]._id.getTimestamp());
     });
 
-    //db.close();   // Due to mongodb npm library V3 its deprecated.
+    // Due to mongodb npm library V3 its deprecated.
+    //db.close();   
     client.close();
 });
