54c5ffcefaeb5ac9ad6e6980c203cfd7b67a79fc
Vishvang <vishvangp.8@gmail.com>
2018-08-11 16:07:10 -0400

    Create one new Collection Todos and Users and Insert one single document in each collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,39 @@
 const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true},
-(err, db) => {
+(err, client) => {
     if(err) {
         return console.log('Unable to coonect with MongoDB server');
     }
-    console.log('connected to MongoDB server');
+    console.log('Connected To MongoDB server');
 
-    db.close();
+    // Database instance.
+    const db = client.db('TodoApp');
+    
+    //Create one new Collection Todos and Insert one single record
+    db.collection('Todos').insertOne({
+        Text:'Something to do',
+        completed: false
+    },(err, result) => {
+        if(err){
+            console.log('Unable to insert document into Todos Collection', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    //Create one new Collection Users and Insert one single record
+    db.collection('Users').insertOne({
+        name: 'Vish',
+        age: 27,
+        location: 'Tampa'
+    },(err, result) => {
+        if(err){
+            console.log('Unable to insert document into Todos Collection', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    //db.close();   // Due to mongodb npm library V3 its deprecated.
+    client.close();
 });
